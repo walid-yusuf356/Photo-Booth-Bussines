522b40034de65fafff8fb4dd05523f1899f8d3b5
walid-yusuf356 <walidyusuf@gmail.com>
2025-01-10 23:17:24 -0600

    updated home page

--- src/pages/Gallery/gallery.jsx
+++ src/pages/Gallery/gallery.jsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 import "./gallery.css";
 
 // Import all images
-import img1 from "../assets/images/gallery/gallery-1.jpg";
-import img2 from "../assets/images/gallery/gallery-2.jpg";
-import img3 from "../assets/images/gallery/gallery-3.jpg";
-import img4 from "../assets/images/gallery/gallery-4.jpg";
-import img5 from "../assets/images/gallery/gallery-5.jpg";
-import img6 from "../assets/images/gallery/gallery-6.jpg";
-import img7 from "../assets/images/gallery/gallery-7.jpg";
-import img8 from "../assets/images/gallery/gallery-8.jpg";
-import img9 from "../assets/images/gallery/gallery-9.jpg";
-
+import img1 from "../../assets/images/gallery/gallery-1.jpg";
+import img2 from "../../assets/images/gallery/gallery-2.jpg";
+import img3 from "../../assets/images/gallery/gallery-3.jpg";
+import img4 from "../../assets/images/gallery/gallery-4.jpg";
+import img5 from "../../assets/images/gallery/gallery-5.jpg";
+import img6 from "../../assets/images/gallery/gallery-6.jpg";
+import img7 from "../../assets/images/gallery/gallery-7.jpg";
+import img8 from "../../assets/images/gallery/gallery-8.jpg";
+import img9 from "../../assets/images/gallery/gallery-9.jpg";
 const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9];
 
 function Gallery() {
