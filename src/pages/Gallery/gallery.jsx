77e6ab86f7d62f625caed1c87870a850d144f2be
walid-yusuf356 <walidyusuf@gmail.com>
2025-01-10 23:46:59 -0600

    updated gallery page

--- src/pages/Gallery/gallery.jsx
+++ src/pages/Gallery/gallery.jsx
@@ -11,36 +11,42 @@ import img6 from "../../assets/images/gallery/gallery-6.jpg";
 import img7 from "../../assets/images/gallery/gallery-7.jpg";
 import img8 from "../../assets/images/gallery/gallery-8.jpg";
 import img9 from "../../assets/images/gallery/gallery-9.jpg";
+
 const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9];
 
 function Gallery() {
   const [selectedImg, setSelectedImg] = useState(null);
 
-  // Function to handle clicking outside the image to close the modal
-  const handleCloseModal = (e) => {
-    if (e.target.classList.contains("modal")) {
-      setSelectedImg(null);
-    }
-  };
-
   return (
-    <div className="gallery">
-      {images.map((img, index) => (
-        <img
-          key={index}
-          src={img}
-          alt={`Gallery Image ${index + 1}`}
-          className="gallery-image"
-          onClick={() => setSelectedImg(img)}
-        />
-      ))}
+    <div className="gallery-section">
+      <h1 className="gallery_title">Our Gallery</h1>
+      <p className="gallery_description">
+        Check out our gallery to see our photo booth in action!
+      </p>
+
+      <div className="gallery-grid">
+        {images.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt={`Gallery Image ${index + 1}`}
+            className="gallery-image"
+            onClick={() => setSelectedImg(img)}
+          />
+        ))}
+      </div>
 
       {selectedImg && (
-        <div className="modal" onClick={handleCloseModal}>
-          <span className="close" onClick={() => setSelectedImg(null)}>
-            &times;
-          </span>
-          <img src={selectedImg} alt="Enlarged" className="enlarged-image" />
+        <div className="popup-overlay" onClick={() => setSelectedImg(null)}>
+          <div className="popup-content">
+            <button
+              className="popup-close"
+              onClick={() => setSelectedImg(null)}
+            >
+              &times;
+            </button>
+            <img src={selectedImg} alt="Enlarged" className="popup-img" />
+          </div>
         </div>
       )}
     </div>
